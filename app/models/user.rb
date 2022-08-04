b2c4836a9c51660f6213cd0da911c66f942a00b8
Hunter Kassow <hunterkassow@protonmail.com>
2022-08-04 11:41:39 -0400

    create user works correctly

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
     has_many :reviews
     has_many :comics, through: :reviews
     has_secure_password
-    validates :name, :username, presence: true
+    validates :username, presence: true
     validates :username, uniqueness: true
 end
